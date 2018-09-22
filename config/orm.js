d61a02154958b6048b026f64a0b452ba01c327a4
alex-bowen <alexbowen2017@gmail.com>
2018-09-21 21:34:22 -0500

    Largely working, trying to program button

--- config/orm.js
+++ config/orm.js
@@ -1,15 +1,102 @@
 // import MySql connetion
 var connection = require("../config/connection.js");
 
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations 
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {burger_name: 'Bacon Cheeseburger'} => ["burger_name='Bacon Cheeseburger'"]
+            // e.g. {devoured: true} => ["devoured=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+
 // Create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
 
 var orm = {
     // selectAll();
-    
-    // insertOne();
-    
-    // updateOne();
-}
+    selectAll: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
 
-// export
-module.exports = orm;
+    // insertOne();
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
+    // updateOne();
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
+
+      // delete();
+    //   delete: function(table, condition, cb) {
+    //     var queryString = "DELETE FROM " + table;
+    //     queryString += " WHERE ";
+    //     queryString += condition;
+    
+    //     connection.query(queryString, function(err, result) {
+    //       if (err) {
+    //         throw err;
+    //       }
+    
+    //       cb(result);
+    //     });
+    //   }
+    };
+    
+    // Export the orm object for the model (burger.js).
+    module.exports = orm;
+    
--- models/burger.js
+++ models/burger.js
@@ -4,9 +4,31 @@ var orm = require("../config/orm.js");
 
 // Create the code that will call the ORM functions using burger specific input for the ORM.
 
-var burger = {
-    // call ORM functions using specific input
-}
-
 // export
-module.exports = burger;
+
+var burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+    // ,
+    // delete: function(condition, cb) {
+    //   orm.delete("burgers", condition, function(res) {
+    //     cb(res);
+    //   });
+    // }
+  };
+  
+  module.exports = burger;
